43fe8abb38bbef10188914aef5a40af1cdd82471
yuchiwang <921685186@qq.com>
2021-01-04 15:29:56 +0800

    git init

---
+++ src/App.vue
@@ -0,0 +1,6 @@
+<template>
+  <div id="app">
+    <div id="nav" />
+    <router-view />
+  </div>
+</template>
---
+++ src/components/ElementUI/index.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import { Button, Select } from 'element-ui'
+import '@/components/ElementUI'
+
+Vue.use(Button)
+Vue.use(Select)
